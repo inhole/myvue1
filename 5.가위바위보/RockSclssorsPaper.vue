1a3c31d7da101938f3eb99ad42a78b266ae55af5
Ino <sylee74133@gmail.com>
2024-06-10 16:39:30 +0900

    연습) 가위바위보 추가-2

--- 5.가위바위보/RockSclssorsPaper.vue
+++ 5.가위바위보/RockSclssorsPaper.vue
@@ -1,15 +1,109 @@
 <template>
   <div>
+    <div id="computer" :style="computedStyleObject"></div>
+    <div>
+      <button @click="onClickButton('바위')">바위</button>
+      <button @click="onClickButton('가위')">가위</button>
+      <button @click="onClickButton('보')">보</button>
+    </div>
+    <div>{{result}}</div>
+    <div>현재 {{score}}점</div>
+    <lifecycle-example v-if="true"/>
   </div>
 </template>
 
 <script>
+  const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+  };
+
+  const scores = {
+    가위: 1,
+    바위: 0,
+    보: -1,
+  };
+
+  const computerChoise = (imgCoord) => {
+    return Object.entries(rspCoords).find(function (v) {
+      return v[1] === imgCoord;
+    })[0];
+  };
+
+  let interval = null;
+
   export default {
     data() {
+      return {
+        imgCoord: rspCoords.바위,
+        result: '',
+        score: 0,
+      }
     },
     computed: {
+        computedStyleObject() {
+          return {
+            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
+          }
+        }
     },
     methods: {
+      changeHand() {
+        interval = setInterval(() => {
+          if (this.imgCoord === rspCoords.바위) {
+            this.imgCoord = rspCoords.가위;
+          } else if (this.imgCoord === rspCoords.가위) {
+            this.imgCoord = rspCoords.보;
+          } else if (this.imgCoord === rspCoords.보) {
+            this.imgCoord = rspCoords.바위;
+          }
+        }, 100);
+      },
+      onClickButton(choice) {
+        clearInterval(interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoise(this.imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+          this.result = '비겼습니다.';
+        } else if ([-1, 2].includes(diff)) {
+          this.result = '이겼습니다.'
+          this.score += 1;
+        } else {
+          this.result = '졌습니다.'
+          this.score -= 1;
+        }
+        setTimeout(() => {
+          this.changeHand();
+        }, 1000);
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreated');
+    },
+    created() { // 화면에 나타나기 전 실행
+      console.log('created');
+    },
+    beforeMount() {
+      console.log('beforeMounted');
+    },
+    mounted() { // 화면에 나타난 후 실행
+      console.log('mounted');
+      this.changeHand();
+    },
+    beforeUpdate() {
+      console.log('beforeUpdated');
+    },
+    updated() { // 화면에 데이터가 바껴서 다시 그려질 때 실행
+      console.log('updated');
+    },
+    beforeDestroy() {
+      console.log('beforeDestroy');
+      clearInterval(interval);
+    },
+    destroyed() { // 화면에 있다가 없어질 때 실행
+      console.log('destroyed');
     },
   }
 </script>
