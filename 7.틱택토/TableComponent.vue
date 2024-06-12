2ef077c38d01e4c3fe4d74e92d27d1eff2dce7ea
Ino <sylee74133@gmail.com>
2024-06-12 22:31:55 +0900

    연습) 틱택토 추가

---
+++ 7.틱택토/TableComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <table>
+    <tr-component v-for="(rowData, index) in tableData" :key="index" :row-data="rowData" :row-index="index"></tr-component>
+  </table>
+</template>
+
+<script>
+  import TrComponent from './TrComponent';
+  export default {
+    props: {
+      tableData: Array, // TicTacToe.vue 에서 받은 tableData prop 선언
+    },
+    components: { // 자식 컴포넌트
+      TrComponent,
+    },
+  };
+</script>
---
+++ 7.틱택토/webpack.config.js
@@ -0,0 +1,34 @@
+// node 환경
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // 개발 : development, 배포 : production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue'], // 확장자 처리
+    },
+    entry: {
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: {
+        rules:[{
+            test: /\.vue$/,
+            use: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+            ]
+        }],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist',
+    },
+}
