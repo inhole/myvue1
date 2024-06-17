a59f00435a8deb97a10a7db68ca7581149529d45
Ino <sylee74133@gmail.com>
2024-06-17 22:13:10 +0900

    연습) 틱택토Vuex 추가 : Vuex는 루트 파일에서 데이터 관리하게 해줌

---
+++ 7.틱택토Vuex/TableComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <table>
+    <tr-component v-for="(rowData, index) in tableData" :key="index" :row-index="index"></tr-component>
+  </table>
+</template>
+
+<script>
+  import TrComponent from './TrComponent';
+  export default {
+    components: { // 자식 컴포넌트
+      TrComponent,
+    },
+    computed: {
+      tableData() {
+        return this.$store.state.tableData;
+      },
+    },
+  };
+</script>
---
+++ 7.틱택토Vuex/main.js
@@ -0,0 +1,6 @@
+// vue 환경
+import Vue from 'vue';
+
+import TicTacToe from "./TicTacToe.vue";
+
+new Vue(TicTacToe).$mount('#root');
