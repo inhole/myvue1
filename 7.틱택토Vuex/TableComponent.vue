29f050b780b1c996bdbfbd3eeaf492d488926f62
Ino <sylee74133@gmail.com>
2024-06-18 20:34:45 +0900

    연습) 틱택토Vuex : slot 추가

--- 7.틱택토Vuex/TableComponent.vue
+++ 7.틱택토Vuex/TableComponent.vue
@@ -1,19 +1,11 @@
 <template>
   <table>
-    <tr-component v-for="(rowData, index) in tableData" :key="index" :row-index="index"></tr-component>
+    <slot />
   </table>
 </template>
 
 <script>
-  import TrComponent from './TrComponent';
   export default {
-    components: { // 자식 컴포넌트
-      TrComponent,
-    },
-    computed: {
-      tableData() {
-        return this.$store.state.tableData;
-      },
-    },
+
   };
 </script>
